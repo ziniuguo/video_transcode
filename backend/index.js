ff8b48ab2521726be67ca27254c8a291134ee17c
ziniu <guo.ziniu.1003@gmail.com>
2024-10-06 20:53:49 +0800

    fix cookie

--- backend/index.js
+++ backend/index.js
@@ -163,6 +163,8 @@ app.post('/login', async (req, res) => {
         const idToken = authResponse.AuthenticationResult.IdToken;
         const accessToken = authResponse.AuthenticationResult.AccessToken;
 
+        console.log("login cookie: ", idToken, accessToken);
+
         // 设置 HttpOnly Cookies，前端无法通过 JavaScript 访问
         res.cookie('idToken', idToken, {sameSite: 'lax', httpOnly: true, secure: false, path:'/' });
         res.cookie('accessToken', accessToken, {sameSite: 'lax', httpOnly: true, secure: false , path:'/'});
